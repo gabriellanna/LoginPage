27c2715492b338118db876ae7ced8578d96613f8
gabriel <alveslanna97@gmail.com>
2023-01-05 20:05:55 -0300

    Modelo DashBoard

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Body from '../../components/Body/Body';
+
+const ProfilePage = () => {
+    return (
+        <Body>
+            <div><h1>Página de Perfil</h1></div>
+        </Body>
+    )
+}
+
+export default ProfilePage;
